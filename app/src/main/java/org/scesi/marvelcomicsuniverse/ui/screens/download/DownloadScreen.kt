1193cddc9bc4521698e0920c64e5a04a1c091d7e
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-06-20 13:22:46 -0400

    feat(navigation): add navigation between 'HomeScreen' and 'DownloadScreen' screens by button

--- app/src/main/java/org/scesi/marvelcomicsuniverse/ui/screens/download/DownloadScreen.kt
+++ app/src/main/java/org/scesi/marvelcomicsuniverse/ui/screens/download/DownloadScreen.kt
@@ -1,18 +1,25 @@
 package org.scesi.marvelcomicsuniverse.ui.screens.download
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.scesi.marvelcomicsuniverse.ui.navigation.NavRoutes
 import org.scesi.marvelcomicsuniverse.ui.theme.MarvelComicsUniverseTheme
 
 @Composable
-fun DownloadScreen() {
+fun DownloadScreen(
+    navHostController: NavHostController
+) {
     MarvelComicsUniverseTheme {
         Scaffold { innerPadding ->
             Box(
@@ -21,7 +28,12 @@ fun DownloadScreen() {
                     .padding(innerPadding),
                 contentAlignment = Alignment.Center
             ) {
-                Text(text = "DownloadScreen")
+                Column {
+                    Text(text = "DownloadScreen")
+                    Button(onClick = {navHostController.navigate(NavRoutes.HOME_SCREEN)}) {
+                        Text(text = "Home")
+                    }
+                }
             }
         }
     }
@@ -30,5 +42,6 @@ fun DownloadScreen() {
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun DownloadScreenPreview() {
-    DownloadScreen()
+    val navHostController = rememberNavController()
+    DownloadScreen(navHostController)
 }
