e98ed3ecf6750ac145b4397f457b443198194432
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-06-22 23:46:20 -0400

    feat(MainActivity): apply the theme 'MarvelComicsUniverseTheme' and the Scaffold

--- app/src/main/java/org/scesi/marvelcomicsuniverse/MainActivity.kt
+++ app/src/main/java/org/scesi/marvelcomicsuniverse/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import org.scesi.marvelcomicsuniverse.ui.components.BottomBarNav
 import org.scesi.marvelcomicsuniverse.ui.theme.MarvelComicsUniverseTheme
 import org.scesi.marvelcomicsuniverse.ui.navigation.Navigation
 
@@ -20,8 +18,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navHostController = rememberNavController()
-            Navigation(navHostController = navHostController)
+            val navController = rememberNavController()
+            MarvelComicsUniverseTheme {
+                Scaffold(
+                    bottomBar = { BottomBarNav(navController) }
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Navigation(navController)
+                    }
+                }
+            }
         }
     }
 }
