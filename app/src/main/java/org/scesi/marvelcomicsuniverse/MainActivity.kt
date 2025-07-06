99ca90434e25c204f6fb8b3f52c781d0fa06cfcd
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-06 02:50:45 -0400

    feat(top-bar): concealable top bar

--- app/src/main/java/org/scesi/marvelcomicsuniverse/MainActivity.kt
+++ app/src/main/java/org/scesi/marvelcomicsuniverse/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
 import org.scesi.marvelcomicsuniverse.ui.components.BottomBarNav
 import org.scesi.marvelcomicsuniverse.ui.components.TopBar
@@ -16,14 +20,18 @@ import org.scesi.marvelcomicsuniverse.ui.theme.MarvelComicsUniverseTheme
 import org.scesi.marvelcomicsuniverse.ui.navigation.Navigation
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
             MarvelComicsUniverseTheme {
                 Scaffold(
-                    topBar = { TopBar() },
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    topBar = { TopBar(scrollBehavior = scrollBehavior) },
                     bottomBar = { BottomBarNav(navController) }
                 ) { innerPadding ->
                     Box(
