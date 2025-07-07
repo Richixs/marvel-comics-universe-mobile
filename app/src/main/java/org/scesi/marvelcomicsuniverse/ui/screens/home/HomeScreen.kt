898430f11bea18bef5f1bccfb2ef6c0f0ebd3806
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-06 22:32:22 -0400

    feat(ui/home): add 'ComicItem'

--- app/src/main/java/org/scesi/marvelcomicsuniverse/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/scesi/marvelcomicsuniverse/ui/screens/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,7 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -21,7 +25,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.scesi.marvelcomicsuniverse.ui.marvel.components.CharacterItem
 import org.scesi.marvelcomicsuniverse.ui.marvel.components.ComicItem
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.ui.text.font.FontWeight
+import kotlin.math.absoluteValue
 
 @Composable
 fun HomeScreen(
@@ -35,6 +42,40 @@ fun HomeScreen(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(bottom = 56.dp)
     ) {
+        item {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                val pagerState = rememberPagerState(pageCount = { comics.size })
+                HorizontalPager(
+                    state = pagerState,
+                    contentPadding = PaddingValues(horizontal = 16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                ) { page ->
+                    val pageOffset =
+                        (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
+                    val scale = 1f - pageOffset.absoluteValue * 0.25f
+                    ComicItem(
+                        comic = comics[page],
+                        modifier = Modifier
+                            .graphicsLayer {
+                                val pageOffset =
+                                    (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
+                                translationX = pageOffset * (180.dp.toPx())
+                                alpha = 1f - pageOffset.absoluteValue.coerceIn(0f, 1f)
+                                val scale = 1f - pageOffset.absoluteValue * 0.3f
+                                scaleX = scale
+                                scaleY = scale
+                                rotationZ = pageOffset * 15f
+                            }
+                    )
+                }
+            }
+        }
         item {
             Text(
                 text = "Characters",
